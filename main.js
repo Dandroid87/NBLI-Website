0698f36d3f0bc596447427b6dd2c39fad082e964
The No Block Limit Initiative <38795952+noblocklimit@users.noreply.github.com>
2018-05-09 22:32:11 -0700

    Created basic JS object

--- main.js
+++ main.js
@@ -1,1 +1,52 @@
 //JS Code goes here.
+
+var bchData  {
+	var pools = [
+		{name:"ViaBTC", hashrate:, website:"viabtc.com", twitter:"viabtc", email:"support@viabtc.com", isSupport:false, proof:"", logo:""},
+		{name:"BTC.top", hashrate:, website:"", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+		{name:"Bitcoin.com", hashrate:, website:"pool.bitcoin.com", twitter:"bitcoincom", email:"pool@bitcoin.com", isSupport:false, proof:"", logo:""},
+		{name:"Antpool", hashrate:, website:"antpool.com", twitter:"AntPoolofficial", email:"", isSupport:false, proof:"", logo:""},
+		{name:"BTC.com", hashrate:, website:"btc.com", twitter:"@btccom_official", email:"", isSupport:false, proof:"", logo:""},
+		{name:"Rawpool", hashrate:, website:"pool.rawpool.co", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+		{name:"Coingeek", hashrate:, website:"", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+		{name:"SBI Crypto", hashrate:, website:"", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+		{name:"F2Pool", hashrate:, website:"", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+		{name:"", hashrate:, website:"", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+
+
+
+	],
+
+	var devTeams =  [
+		{name:"BitcoinABC", website:"bitcoinabc.org", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+		{name:"Bitcoin Unlimited", website:"bitcoinunlimited.info", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+		{name:"Bitcoin XT", website:"bitcoinXT.software", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+		{name:"NChain", website:"", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+		{name:"Parity Bitcoin", website:"", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+		{name:"Bitprim", website:"", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+		{name:"Electron Cash", website:"", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+		{name:"", website:"", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+		{name:"", website:"", twitter:"", email:"", isSupport:false, proof:"", logo:""}
+
+
+	]/*,
+
+	var companies = [
+		{name:"", website:"", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+		{name:"", website:"", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+		{name:"", website:"", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+		{name:"", website:"", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+		{name:"", website:"", twitter:"", email:"", isSupport:false, proof:"", logo:""}
+
+	],
+
+	var individuals = [
+		{name:"", website:"", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+		{name:"", website:"", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+		{name:"", website:"", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+		{name:"", website:"", twitter:"", email:"", isSupport:false, proof:"", logo:""},
+		{name:"", website:"", twitter:"", email:"", isSupport:false, proof:"", logo:""}
+
+	] 
+	*/
+};
